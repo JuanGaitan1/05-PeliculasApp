e09d40973338ad5a831c6ebf48c750918ae2b188
Juan Gaitan <juanigaitan7@gmail.com>
2023-05-23 14:37:38 -0300

    2da Clase

--- src/componenst/MoviePoster.tsx
+++ src/componenst/MoviePoster.tsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import { Image, View, Text, StyleSheet } from 'react-native';
-import { Movie } from '../interfaces/movieinterface';
+import { Movie } from '../interfaces/movieInterface';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface Props {
     movie: Movie;
     height?: number;
     width?:number;
-
 }
+
+
+
 
 export const MoviePoster = ({movie, height= 420, width= 300}: Props) => {
 
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
+    const navigation = useNavigation<any>();
+
   return (
-    <View style= {{ width, height, marginHorizontal: 5}}>
+    <TouchableOpacity 
+        onPress={
+            ()=> navigation.navigate("DetailScreen", movie)
+        }
+        activeOpacity={0.8}
+            style= {{
+                width,
+                height,
+                marginHorizontal: 2,
+                paddingBottom:20,
+                paddingHorizontal: 5, 
+            }}>
         <View   style= {styles.imageContainer }>
 
             <Image
@@ -23,7 +40,7 @@ export const MoviePoster = ({movie, height= 420, width= 300}: Props) => {
             />
 
         </View>
-    </View>
+    </TouchableOpacity>
   )
 }
 
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,12 +1,75 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { RootStackParams } from '../navigation/Navigation';
+import { ScrollView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons'
+import { useMovieDetails } from '../hooks/useMovieDetails';
 
-export const DetailScreen = () => {
+const screenHeight =  Dimensions.get('screen').height
+
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'>{};
+
+
+
+
+export const DetailScreen = ({ route }: Props) => {
+
+  const movie= route.params
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+
+  const { isLoading, cast, movieFull} = useMovieDetails(movie.id)
+  
+  
   return (
-    <View>
-        
-        <Text> Detail Screen </Text>
+    <ScrollView>
+        <View style={styles.imageConteiner}>
 
-    </View>
+            <Image
+                source={{uri}}
+                style={styles.PosterImage}
+            />
+
+        </View>
+        <View style= {styles.marginContainer}>
+            <Text style={styles.subTitle}>{movie.original_title}</Text>
+            <Text style={styles.title}>{movie.title}</Text>
+        </View>
+        <View style= {styles.marginContainer}>
+           <Icon 
+           name="star-outline"
+           color= 'grey'
+           size={20}
+           
+           />
+        </View>
+
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  PosterImage:{
+    flex: 1,
+    
+  },
+  imageConteiner:{
+    width: '100%',
+    height: screenHeight * 0.7,
+    overflow: 'hidden',
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25
+  },
+  marginContainer:{
+    marginHorizontal:20,
+    marginTop: 20,
+  },
+  subTitle:{
+  fontSize: 16,
+  opacity:0.8
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+})
